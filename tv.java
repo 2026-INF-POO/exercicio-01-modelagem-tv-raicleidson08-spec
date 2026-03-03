fa497342dc3d9cacf35b4f0cf40891fc851326fb
raicleidson08-spec <raicleidson08@gmail.com>
2026-03-03 09:10:28 -0300

    Update and rename README.md to tv.java

--- tv.java
+++ tv.java
@@ -1,10 +1,10 @@
-# Exercício: Modelagem TV
+
 
 1. Edite o arquivo `TV.java` e construa uma modelagem para representar uma TV utilizando as informações abaixo. 
 
     Uma televisão possui as seguintes características:
     - tamanho de tela (em polegadas);
-    - volume: de 1 a 10 iniciando em 5;
+    - volume: de 100 a 1 iniciando em 5;
     - marca;
     - voltagem (220 e 110);
     - canal.
@@ -14,3 +14,17 @@
     - desligar;
     - aumentar e diminuir o volume;
     - subir e descer canal.
+       class TV{
+           int tamanho;
+           int volume = 5;
+           string marca;
+           int voltagem;
+           int canal;
+
+           int aumentarvolume(){
+               if (volume > 100){
+                   volume--;
+               }
+               return volume;
+           }
+    }
